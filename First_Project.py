9076b983bc1f65a456cbc2300bea675465d501a4
amir7212 <mauliev@mail.ru>
2023-11-30 17:38:39 +0600

    Проект готов

--- First_Project.py
+++ First_Project.py
@@ -1,8 +1,15 @@
-class To_do_list():
-    def __init__(self,name,description):
-        self.name=name
-        self.description=description
-   
-
-
-    
+from Project2 import *
+answer = int(input("Выберите действие\n 1. Добавить или отредактировать задачу\n 2. Просмотреть все задачи\n 3. Удалить задачу\n 4. Выйти\n "))
+while answer != 4:
+    if answer == 1:
+        print("Добавить или отредактировать задачу")
+        add_task(tasks)
+    elif answer == 2:
+        print("Просмотреть все задачи")
+        display_info(tasks)
+    elif answer == 3:
+        print("Удалить задачу")
+        del_task(tasks)
+    else:
+        print("Введите еще раз")
+    answer = int(input("Выберите действие\n 1. Добавить или отредактировать задачу\n 2. Просмотреть все задачи\n 3. Удалить задачу\n 4. Выйти\n "))
